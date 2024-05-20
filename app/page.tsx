a74d9eab5684506a4d9165b077f6af9679f1e583
dmitridrozdov <dm.drozdov@gmail.com>
2024-05-20 14:53:46 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
         <h1>hello portfolio</h1>
       </div>
